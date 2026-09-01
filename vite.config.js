77111f9b81615ca603af56b4ccb2db762546ccd1
axlesholtz13 <alex.robinson@gov.bc.ca>
2026-09-01 10:07:20 -0700

    Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // set base to "/<repo>/". For a user/root site or local dev, keep "/".
 export default defineConfig({
   plugins: [react()],
-  base: "/axlesholtz13/kml2shape",
+  base: "/kml2shape/",
 });
